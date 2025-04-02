7e8c06a854589b056d57be52e90230b776e81e47
ABHISHEK RAJPUT <79101220+thakur06@users.noreply.github.com>
2025-04-02 12:30:26 +0530

    removed test folder

--- Test/event_loop.js
+++
@@ -1,8 +0,0 @@
-"http://latentflip.com/loupe/"
-
-
-"Simple tasks -- > callback stack and the web api --> goes to the task queue"
-
-
-" --> then after the timeout pushed to the calback stack for execution "
-
--- Test/network/vite.config.js
+++
@@ -1,9 +0,0 @@
-
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
